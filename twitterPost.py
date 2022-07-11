a4d00a7a0c6a4dd1d38fbf25db965f1418211f1c
abnor <ddarktemplar01@gmail.com>
2022-07-11 12:13:55 +0300

    twitterPost script tweeting sales

---
+++ twitterPost.py
@@ -0,0 +1,32 @@
+import tweepy
+import os
+import requests
+
+class poster:
+    def twitter_api(self):
+        #your twitter API keys & secrets
+        access_token = ''
+        access_token_secret = ''
+        consumer_key = ''
+        consumer_secret = ''
+                           
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+        self.api = api
+
+    def tweet_image(self,url, message):
+        
+        filename = 'tempmindfolk.jpg'
+        request = requests.get(url, stream=True)
+        if request.status_code == 200:
+            with open(filename, 'wb') as image:
+                for chunk in request:
+                    image.write(chunk)
+
+            media = self.api.media_upload(filename)
+            os.remove(filename)
+            post_result = self.api.update_status(status=message, media_ids=[media.media_id])
+        else:
+            print("Unable to download image")
+
